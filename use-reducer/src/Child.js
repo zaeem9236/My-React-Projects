fc6c9ed76b5d60edd66196174d344d604abd16e8
zaeem9236 <60064010+zaeem9236@users.noreply.github.com>
2020-10-21 02:30:53 +0500

    parent and child components made

---
+++ use-reducer/src/Child.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Child(){
+    return(
+        <React.Fragment>
+            <h1>hireachy complete</h1>
+        </React.Fragment>
+    );
+}
+
+export default Child;
