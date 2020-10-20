0c73d7171fa03c93c04608cb048435a197ba9543
zaeem9236 <60064010+zaeem9236@users.noreply.github.com>
2020-10-21 02:38:12 +0500

    use-reducer biolerplate created

---
+++ use-reducer/src/AppReducer.js
@@ -0,0 +1,12 @@
+
+const reducer = (state, action)=>{
+    switch (action){
+        case 'Increment':
+            return state + 1
+
+        case 'Decrement':
+            return state -1
+    }
+}
+
+export default reducer;
--- use-reducer/src/Child.js
+++ use-reducer/src/Child.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useReducer } from 'react';
+import AppReducer from './AppReducer';
 
 function Child(){
+    let [state, dispatch] = useReducer(AppReducer,0);
     return(
         <React.Fragment>
-            <h1>hireachy complete</h1>
+            <h1>value: {state}</h1>
+            <button onClick={()=>{dispatch('Increment')}}>Increment</button>
+            <button onClick={()=>{dispatch('Decrement')}}>Decrement</button>
         </React.Fragment>
     );
 }
