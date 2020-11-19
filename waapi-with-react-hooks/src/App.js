7003768c2314dac0b1163d5f8529392b8f206125
zaeem9236 <60064010+zaeem9236@users.noreply.github.com>
2020-11-19 18:44:27 +0500

    waapi with react hooks updated

---
+++ waapi-with-react-hooks/src/App.js
@@ -0,0 +1,43 @@
+import React, {useEffect} from 'react';
+import './App.css';
+import useWebAnimations from "@wellyshen/use-web-animations";
+
+const App = () => {
+  const { ref, animate } = useWebAnimations();
+
+  useEffect(()=>{
+    animate({
+      keyframes: {
+        transform: ["translateX(500px)"], // Move by 500px
+        background: ["red", "blue", "green"], // Go through three colors
+      },
+      timing: {
+        delay: 500, // Start with a 500ms delay
+        duration: 1000, // Run for 1000ms
+        iterations: 2, // Repeat once
+        direction: "alternate", // Run the animation forwards and then backwards
+        easing: "ease-in-out", // Use a fancy timing function
+      },
+      onReady: ({ playState, animate, animation }) => {
+        // Triggered when the animation is ready to play (Google Chrome: available in v84+)
+      },
+      onUpdate: ({ playState, animate, animation }) => {
+        // Triggered when the animation enters the running state or changes state
+      },
+      onFinish: ({ playState, animate, animation }) => {
+        // Triggered when the animation enters the finished state (Google Chrome: available in v84+)
+      },
+      // More useful options...
+    });
+  },[]);
+  
+
+  return (
+    <div className="container">
+      <p>🍿 Animation  with Waapi and React hooks</p>
+      <div className="target" ref={ref} />
+    </div>
+  );
+};
+
+export default App
